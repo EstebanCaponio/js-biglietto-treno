1255a752ba92d9020b9a67d838948cfbef85f16f
EstebanCaponio <estebancaponio@gmail.com>
2025-02-24 17:14:09 +0100

    feat: created input

---
+++ script.js
@@ -0,0 +1,8 @@
+//chiedere all'utente quanti km vuole percorrere
+let distance = prompt("Quanti km vuoi percorre?");
+//stampare il risultato
+console.log(parseInt(distance));
+//chiedere l'età del passeggero
+let age = prompt("quanti anni hai?");
+//stampare il risultato
+console.log(parseInt(age));
